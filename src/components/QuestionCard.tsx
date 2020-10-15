db4e692ae3cdc9b722bf6def51ba4d275fa7ea3d
Abraham Vega <abrahamvega987@gmail.com>
2020-10-15 17:42:54 -0500

    TypeError after open the browser again fixed

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,20 +1,48 @@
 import React, { useContext, useEffect } from "react";
 import { Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../context/auth/AuthContext";
+import FruitsContext from "../context/frutos/FruitsContext";
 import QuestionsContext from "../context/questions/QuestionsContext";
 
 const QuestionCard: React.FC = () => {
   const questionsContext = useContext(QuestionsContext);
+  const authContext = useContext(AuthContext);
+
+  const { saveCredentials } = authContext;
+
+  const frutosContext = useContext(FruitsContext);
+
+  const history = useHistory();
   const { questions, getQuestions, actualIndex } = questionsContext;
 
   useEffect(() => {
     getQuestions();
     // eslint-disable-next-line
-  });
+  }, []);
+
+  const startAgain = () => {
+    localStorage.clear();
+    frutosContext.resetFrutosState();
+    questionsContext.resetStateQuestions();
+    saveCredentials("", "");
+    history.push("/");
+  };
+
+  function showQuestions() {
+    if (questions.length !== 0 && actualIndex <= questions.length - 1) {
+      return questions[actualIndex].question;
+    } else {
+      console.log("aqui toy");
+      startAgain();
+      return <h1>No question available</h1>;
+    }
+  }
 
   return (
     <Card className="mb-3">
       <Card.Body style={{ background: "#90c73f" }}>
-        <h2>{questions.length !== 0 && questions[actualIndex].question}</h2>
+        <h2>{showQuestions()}</h2>
       </Card.Body>
     </Card>
   );
