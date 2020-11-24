9b416701944bb69a67c57c242151f765047e809d
RBar <barraganroque5@gmail.com>
2020-11-23 20:42:41 -0500

    users count added

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,7 +1,7 @@
 import { auth, db } from "../../firebase";
 import { SignUp } from "./interfaces";
 import { UserModel } from "../../models/user_data_model";
-
+import CountModel from '../../models/count_model';
 export const getUser = async (signUpData: SignUp) => {
   try {
     const usuariosRef = db.collection("usuarios");
@@ -25,6 +25,8 @@ export const saveData = async (signUpData: SignUp) => {
   try {
     console.log("enviando");
     await db.collection("usuarios").doc().set(signUpData);
+    const count  = await getCount();
+    await saveCount(count.count)
   } catch (error) {
     console.log(error);
     return error;
@@ -40,4 +42,35 @@ export const signIn = async (email: string, password: string) => {
     .catch((err) => {
       return err;
     });
+};
+
+/* eslint-disable */
+export const getCount = async (): Promise<CountModel> => {
+  try {
+    const countRef = db.collection("count");
+    const query = await countRef.get();
+    const count = CountModel.fromQuerySnapshot(query);
+    console.log('en count')
+    console.log(count[0].count)
+    return count[0];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/* eslint-disable */
+export const saveCount = async (count: number) => {
+  try {
+    const newCount = count + 1
+    console.log('salvando count')
+    console.log(newCount)
+    const countModel = new CountModel(newCount)
+    await db.collection("count/").doc('3u6lhiKPmGsUiAtFs0W0').set({
+      count:newCount
+    });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
